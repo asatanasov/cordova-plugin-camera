c7c1bdc7d372fac354e89c1fad972c75b830831f
Anton Atanasov <funluiji@gmail.com>
2020-10-02 13:50:43 +0300

    upload of downloaded mp4 crash (Android 11)

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -34,8 +34,10 @@ import org.apache.cordova.LOG;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Locale;
 
 public class FileHelper {
