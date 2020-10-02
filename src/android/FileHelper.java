a753c23a47f48e04bb6ce32d3b696c21fdd91945
Anton Atanasov <funluiji@gmail.com>
2020-10-02 16:38:54 +0300

    Revert "Revert " upload of downloaded mp4 crash (msf: format)""
    This reverts commit f893d3779c1afeb23ee075bdc538e5e07eb3071f.

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -97,7 +97,7 @@ public class FileHelper {
 
                 // TODO handle non-primary volumes
             }
-            // DownloadsProvider
+            /* // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
 
                 final String id = DocumentsContract.getDocumentId(uri);
@@ -116,10 +116,11 @@ public class FileHelper {
                 } else {
                     return null;
                 }
-            }
+            }*/
             // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
+            else if (isMediaDocument(uri) || isDownloadsDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                docId = docId.replace("msf:", "video:");
                 final String[] split = docId.split(":");
                 final String type = split[0];
 
